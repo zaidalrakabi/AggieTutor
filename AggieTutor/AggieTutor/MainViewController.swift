40e042a09d8e7eb230dc875f78bdaaf61f4bc1af
Zaid Alrakabi <zaidalrakabi@campus-019-227.ucdavis.edu>
2018-11-15 10:33:59 -0800

    Main

---
+++ AggieTutor/AggieTutor/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AggieTutor
+//
+//  Created by Zaid Alrakabi on 11/15/18.
+//  Copyright © 2018 Aggie Tutor. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
