5cf63e36929d540c894dc8d006305bda6dac8412
joselsoloriojr <jsolorioramirez@ucdavis.edu>
2018-11-16 20:04:05 -0800

    created main view controller

--- AggieTutor/AggieTutor/MainViewController.swift
+++ AggieTutor/AggieTutor/MainViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBAction func LoginSignUpPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "LoginSignUp", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "LoginVC")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func JobTutorsPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "JobsTutors", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "TutorsVC")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func ProfilePressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Profile", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "ProfileVC")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func RequestsPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Request", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "RequestVC")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
