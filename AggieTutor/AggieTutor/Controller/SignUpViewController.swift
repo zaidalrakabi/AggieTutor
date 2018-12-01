8be41c2234246ba0483cc3cdd1ef43a05e31eeab
Dolly Ye <1375373964@qq.com>
2018-12-01 12:23:41 -0800

    finish

--- AggieTutor/AggieTutor/Controller/SignUpViewController.swift
+++ AggieTutor/AggieTutor/Controller/SignUpViewController.swift
@@ -15,8 +15,8 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
-    
-override func viewDidLoad() {
+    @IBOutlet weak var errorLabel: UILabel!
+    override func viewDidLoad() {
         super.viewDidLoad()
         view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
     
@@ -24,6 +24,11 @@ override func viewDidLoad() {
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        usernameField.becomeFirstResponder()
+        
     }
     
     
@@ -33,10 +38,18 @@ override func viewDidLoad() {
             (user, error) in
             if error != nil {
                 print(error!)
+                self.handleError(error!)      // use the handleError method
             } else {
                 print("Sign up successfully")
                 self.performSegue(withIdentifier: "toMainController", sender: self)
             }
+        }
+    }
+    func handleError(_ error: Error) {
+        if let errorCode = AuthErrorCode(rawValue: error._code) {
+            // now you can use the .errorMessage var to get your custom error message
+            print(errorCode.errorMessage)
+            self.errorLabel.text = errorCode.errorMessage
         }
     }
     
