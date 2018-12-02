64f73a1c772a0e0714a6959e2635d0f0e439de87
Zaid Alrakabi <zaidalrakabi@campus-009-056.ucdavis.edu>
2018-12-01 16:37:39 -0800

    Update main and add Profile

--- AggieTutor/AggieTutor/Controller/MainViewController.swift
+++ AggieTutor/AggieTutor/Controller/MainViewController.swift
@@ -11,36 +11,31 @@ import Firebase
 
 class MainViewController: UIViewController {
     
+    /*
+     let storyboard = UIStoryboard(name: "Request", bundle: nil)
+     let vc = storyboard.instantiateViewController(withIdentifier: "RequestVC")
+     self.navigationController?.pushViewController(vc, animated: true)
+ */
+    
+    @IBAction func FindTutorPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "JobsTutors", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "TutorsVC")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func FindJobPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "JobsTutors", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "JobsVC")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
     @IBAction func handleLogOut(_ sender: Any) {
         try! Auth.auth().signOut()
         navigationController?.popToRootViewController(animated: true)
         
     }
     
-    @IBAction func DashboardPressed(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Dashboard", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "DashboardVC")
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    @IBAction func JobTutorsPressed(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "JobsTutors", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "TutorsVC")
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    @IBAction func ProfilePressed(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Profile", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "ProfileVC")
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    @IBAction func RequestsPressed(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Request", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "RequestVC")
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-    override func viewDidLoad() {
+
+override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.hidesBackButton = true
