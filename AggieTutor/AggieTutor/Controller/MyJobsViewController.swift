6fd4552e4c1fe5f63a63e75598d4231ede4a54f8
Zaid Alrakabi <zaidalrakabi@gmail.com>
2018-12-06 10:02:24 -0800

    Added to My Jobs VC

--- AggieTutor/AggieTutor/Controller/MyJobsViewController.swift
+++ AggieTutor/AggieTutor/Controller/MyJobsViewController.swift
@@ -7,30 +7,86 @@
 //
 
 import UIKit
+import Firebase
 
 class MyJobsViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var email: UILabel!
-    @IBOutlet weak var status: UILabel!
+    var databaseRefer: DatabaseReference!
+    var databaseHandle: DatabaseHandle!
+    var jobs = [Jobs]()
+    var curJobs = [Jobs]()
+    
+    func getStudents(){
+        databaseRefer = Database.database().reference()
+        let q1 = self.databaseRefer.child("users")
+        let query = q1.child("students")
+        
+        query.observe(.value, with: { (snapshot) in
+            print(snapshot.value ?? "nothing")
+            
+            if(!self.jobs.isEmpty){
+                self.jobs.removeAll()
+            }
+            
+            for child in snapshot.children{
+                let job = child as! DataSnapshot
+                self.jobs.append(Jobs(job: job))
+                print("<<<<<Printing Job>>>>>>")
+                print("\(job)")
+            }
+            
+            DispatchQueue.main.async {
+                self.curJobs = self.jobs
+                self.tableView.reloadData()
+            }
+        })
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.rowHeight = 100
+        getStudents()
+        print("Finished Loading")
         // Do any additional setup after loading the view.
+        
+    }
+
+}
+
+extension MyJobsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return curJobs.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("Creating TableCell\n")
+        let job = curJobs[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyJobCell") as! MyJobCell
+        cell.navigationController = self.navigationController;
+        cell.setJob(job: job)
+        print("<<<<<Printing Job>>>>>>")
+        print("\(job)")
+        
+        return cell
     }
-    */
+    
+}
 
+class MyJobCell: UITableViewCell{
+    
+    @IBOutlet weak var status: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var name: UILabel!
+
+    var navigationController:UINavigationController! = nil
+    
+    func setJob(job: Jobs){
+        name.text = job.name
+        email.text = job.email
+        status.text = job.status
+    }
+    
 }
